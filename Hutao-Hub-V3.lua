40104f712837b7d68cf36222acf8b9f4805bc4ed
Shade-vex <bieudangcong@gmail.com>
2025-05-21 15:40:48 +0700

    Create Hutao-Hub-V3.lua

---
+++ Hutao-Hub-V3.lua
@@ -0,0 +1,5 @@
+-- [made by SLK gaming]
+-- [youtube : https://www.youtube.com/@SLKgamingSSR]
+-- Tôi là người Việt Nam đẹp trai nhất
+-- like and subscribe pls 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Shade-vex/Dead-Rails-V3/refs/heads/main/Hutao-Hub-V3.txt"))()
